f753b86980e7f95212c35bdbe38bfd2eff104270
sibogatovr <67776232+sibogatovr@users.noreply.github.com>
2023-05-11 18:48:58 +0500

    Добавлена команда !poll

--- Discord_Bot/Commands/FunCommands.cs
+++ Discord_Bot/Commands/FunCommands.cs
@@ -1,6 +1,7 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,6 +31,77 @@ namespace Discord_Bot.Commands
             await ctx.Channel.SendMessageAsync(embed: embedMessage);
         }
 
-        
+        [Command("poll")]
+        public async Task PollCommand(CommandContext ctx, int TimeLimit, string Option1, string Option2, string Option3, string Option4, params string[] Question)
+        {
+            var interactivity = ctx.Client.GetInteractivity();
+            TimeSpan timer = TimeSpan.FromSeconds(TimeLimit);
+            DiscordEmoji[] optionEmojis = { DiscordEmoji.FromName(ctx.Client, ":one:", false),
+                                            DiscordEmoji.FromName(ctx.Client, ":two:", false),
+                                            DiscordEmoji.FromName(ctx.Client, ":three:", false),
+                                            DiscordEmoji.FromName(ctx.Client, ":four:", false),};
+
+            string optionsString = optionEmojis[0] + " | " + Option1 + "\n\n" +
+                                   optionEmojis[1] + " | " + Option2 + "\n\n" +
+                                   optionEmojis[2] + " | " + Option3 + "\n\n" +
+                                   optionEmojis[3] + " | " + Option4;
+
+            var pollMessage = new DiscordMessageBuilder()
+                            .AddEmbed(new DiscordEmbedBuilder()
+                            .WithColor(DiscordColor.Azure)
+                            .WithTitle(string.Join(" ", Question))
+                            .WithDescription(optionsString));
+
+           var putReactOn = await ctx.Channel.SendMessageAsync(pollMessage);
+
+            foreach (var emoji in optionEmojis)
+            {
+                await putReactOn.CreateReactionAsync(emoji);
+            }
+
+            var result = await interactivity.CollectReactionsAsync(putReactOn, timer);
+
+            int count1 = 0; 
+            int count2 = 0;
+            int count3 = 0;
+            int count4 = 0;
+
+            foreach (var emoji in result)
+            {
+                if (emoji.Emoji == optionEmojis[0])
+                {
+                    count1++;
+                }
+                if (emoji.Emoji == optionEmojis[1])
+                {
+                    count2++;
+                }
+                if (emoji.Emoji == optionEmojis[2])
+                {
+                    count3++;
+                }
+                if (emoji.Emoji == optionEmojis[3])
+                {
+                    count4++;
+                }
+            }
+            int totalVotes = count1 + count2 + count3 + count4;
+            string resultsString = optionEmojis[0] + ": " + count1 + " Votes \n" +
+                                   optionEmojis[1] + ": " + count2 + " Votes \n" +
+                                   optionEmojis[2] + ": " + count3 + " Votes \n" +
+                                   optionEmojis[3] + ": " + count4 + " Votes \n\n" +
+                                   "Общее количество проголосовавших: " + totalVotes; 
+
+            var resultMessage = new DiscordMessageBuilder()
+                .AddEmbed(new DiscordEmbedBuilder()
+
+                .WithColor(DiscordColor.Green)
+                .WithTitle("Результат голосания")
+                .WithDescription(resultsString)
+                );
+
+            await ctx.Channel.SendMessageAsync(resultMessage);
+        }
+
     }
 }
